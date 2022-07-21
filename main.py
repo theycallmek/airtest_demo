4ac3529a1d56e5178a573264a07bb3a6ce065d98
theycallmek <theycallmekenny@gmail.com>
2022-07-21 15:12:55 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from airtest.core.api import *
+from poco.drivers.android.uiautomation import AndroidUiautomationPoco
+import time
+
+# Init poco and start our execution time clock.
+poco = AndroidUiautomationPoco(use_airtest_input=True, screenshot_each_action=False)
+total_t0 = time.time()
+
+# Connect to first device listed from 'adb devices'. Specify a device if you want just check the docs.
+try:
+    auto_setup(__file__, logdir=False,
+               devices=[f'Android:///?cap_method=javacap&touch_method=adb', ])
+except IndexError:
+    print('Could not connect to device.')
+
+
+# Using this freeze function allows you to snapshot the UI Hierarchy and store it locally.
+# This dramatically reduces time between clicks because we arent refreshing and pulling that
+# data over adb after every click. It's about a 10x speed increase. If the screen changes you obviously
+# would need to refreeze. That's why the first click takes so much longer to execute.
+with poco.freeze() as frozen_poco:
+    for item in frozen_poco("cookieAnchor"):
+
+        # Click our match 10 times.
+        for i in range(10):
+            i_loop_t0 = time.time()
+            item.click()
+            i_loop_t1 = time.time()
+
+            # Print times clicked and time to click
+            print(f'{item} clicked {i} amount of times')
+            print(f'Time to click: {i_loop_t1 - i_loop_t0}')
+
+# Print total execution time
+total_t1 = time.time()
+print(f'DONE!\nTotal time to execute: {total_t1 - total_t0}')
+print(f'Clicks/second = {(total_t1 - total_t0) / 10}')
+
+# This just fixes a bug I was having where the poco service app was trying to start itself when already running.
+stop_app('com.netease.open.pocoservice')
